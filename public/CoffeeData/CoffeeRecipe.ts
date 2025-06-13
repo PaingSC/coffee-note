064c0114546a367b4f0e35307072c1117cf846d8
PaingSC <paingsoechit.job@gmail.com>
2025-06-13 22:04:48 +0700

    Adding some recipes

--- public/CoffeeData/CoffeeRecipe.ts
+++ public/CoffeeData/CoffeeRecipe.ts
@@ -1,13 +1,42 @@
 export enum CoffeeRoast {
   Light = "Till Noon - Light (Ethiopia)",
   Medium = "Till Midnight - Medium (Brazil Santos)",
-  Dark = "Till Something - Dark (Thailand)",
+  Dark = "No Sleep - Dark (Thailand)",
+  Custom = "Custom beans as the customer preference",
 }
 
-export const Syrup = "syrup(sweetnesse %)" as const;
+export enum Variations {
+  Americano = "Americano",
+  Latte = "Latte",
+  Cuppucino = "Cuppucino",
+  Tea = "Tea",
+  Smoothies = "Smoothies",
+  Frappe = "Frappe",
+  Juice = "Juice",
+  Other = "Other",
+}
+
+export enum Syrup {
+  Syrup = "Syrup",
+  Vanilla = "Vanilla Syrup",
+  Halzelnut = "Halzelnut Syrup",
+  Caramel = "Caramel Syrup",
+  Mint = "Mint Syrup",
+  Strawberry = "Strawberry Fraise",
+}
+
+export enum Milk {
+  Condense = "Condense Milk",
+  Jued = "Milk Jued",
+  FreshMilk = "Fresh Milk",
+}
+
+// export const Syrup = "syrup(sweetnesse %)" as const;
+// export const SpecialSyrup = string as const;
 
 export interface CoffeeRecipe {
   title: string;
-  ingredients: [CoffeeRoast, typeof Syrup, ...string[]];
+  ingredients: [CoffeeRoast, Syrup, ...string[]];
   steps: string[];
+  variations: Variations;
 }
